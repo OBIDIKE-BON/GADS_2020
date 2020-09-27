56e8e59b07ec3deb866ca8f116e0e3ed67eced59
OBIDIKE-BON <bonifaceobidike@gmail.com>
2020-09-27 22:14:52 +0100

    final touches before submission

--- app/src/main/java/com/stackfloat/gads2020/WelcomeActivity.java
+++ app/src/main/java/com/stackfloat/gads2020/WelcomeActivity.java
@@ -27,6 +27,7 @@ public class WelcomeActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(WelcomeActivity.this, MainActivity.class));
+                finish();
             }
         }, 3000);
     }
